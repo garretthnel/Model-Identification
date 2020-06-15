49ffe5aeab84bffb0e8e39c2c4dbf9759aade0e5
Garrett <garretthugonel@gmail.com>
2020-06-15 18:15:58 +0200

    fixed simple error in tsmod

--- tsmod.py
+++ tsmod.py
@@ -4,21 +4,20 @@ import random
 def tabu_search(function, Bounds):
     sBest = numpy.array([random.uniform(*B) for B in Bounds])
     bestCandidate = sBest
-    tabuList = [sBest]
+    tabuList = [list(sBest)]
     maxTabuSize = 5
     min_error = error = function(sBest)
     count = 0
        
     f = 128
 
-    while min_error > 1e-3 and count < f:
+    while min_error > 1e-2 and count < f:
         
         sNeighborhood = numpy.array([numpy.linspace(bc-1, bc+1) for bc in bestCandidate]).T
         bestCandidate = sNeighborhood[0]
-        
+
         for sCandidate in sNeighborhood:
             error = function(sCandidate)
-            
             if (list(sCandidate) not in tabuList) and (error < function(bestCandidate)):
                 bestCandidate = sCandidate
 
@@ -26,13 +25,13 @@ def tabu_search(function, Bounds):
             sBest = bestCandidate
             min_error = function(sBest)
 
-        tabuList.append(bestCandidate)
+        tabuList.append(list(bestCandidate))
 
         if len(tabuList) > maxTabuSize:
             tabuList = tabuList[1:]
 
         count += 1
-        
+
     return sBest
 
 
